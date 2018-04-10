c4a9ee553c9df2e2754125a85c4073d9181face4
bharuka2 <bharuka2@illinois.edu>
2018-04-10 04:11:19 -0500

    initial code for four way

--- l2_cache_control.sv
+++ l2_cache_control.sv
@@ -5,24 +5,34 @@ module l2_cache_control
 	/* Signals to datapath*/
 	output logic valid0_write,
 	output logic valid1_write,
+	output logic valid2_write,
+	output logic valid3_write,
 	output logic valid_in,
 	output logic dirty0_write,
 	output logic dirty1_write,
+	output logic dirty2_write,
+	output logic dirty3_write,
 	output logic dirty_in,
 	output logic tag0_write,
 	output logic tag1_write,
+	output logic tag2_write,
+	output logic tag3_write,
 	output logic data0_write,
 	output logic data1_write,
+	output logic data2_write,
+	output logic data3_write,
 	output logic lru_write,
-	output logic lru_in,
+	output logic [2:0] lru_in,
 	output logic pmem_addr_sig,
 	output logic data_sig,
 	
 	/* Signals from datapath*/
-	input lru_out,
+	input [2:0] lru_out,
 	input dirty_out,
 	input cline0_and,
 	input cline1_and,
+	input cline2_and,
+	input cline3_and,
 	input hit,
 	
 	/* Signals from cpu*/
@@ -62,16 +72,24 @@ begin : state_actions
     /* Default output assignments */
 	 valid0_write = 1'b0;
 	 valid1_write = 1'b0;
+	 valid2_write = 1'b0;
+	 valid3_write = 1'b0;
 	 valid_in = 1'b0;
 	 dirty0_write = 1'b0;
 	 dirty1_write = 1'b0;
+	 dirty2_write = 1'b0;
+	 dirty3_write = 1'b0;
 	 dirty_in = 1'b0;
 	 tag0_write = 1'b0;
 	 tag1_write = 1'b0;
+	 tag2_write = 1'b0;
+	 tag3_write = 1'b0;
 	 data0_write = 1'b0;
 	 data1_write = 1'b0;
+	 data2_write = 1'b0;
+	 data3_write = 1'b0;
 	 lru_write = 1'b0;
-	 lru_in = 1'b0;
+	 lru_in = 3'b000;
 	 pmem_addr_sig = 1'b0;
 	 data_sig = 1'b0;
 	 resp = 1'b0;
@@ -87,11 +105,24 @@ begin : state_actions
 				// Read
 				if (!cpu_write)
 				begin
-					if (cline0_and)
-						lru_in = 1;
-					else if (cline1_and)
-						lru_in = 0;
-						
+					lru_in = lru_out;
+					if (cline0_and) begin
+						lru_in[0] = 0;
+						lru_in[1] = 0;
+					end
+					else if (cline1_and) begin
+						lru_in[0] = 0;
+						lru_in[1] = 1;
+					end
+					else if (cline2_and) begin
+						lru_in[0] = 1;
+						lru_in[2] = 0;
+					end
+					else begin
+						lru_in[0] = 1;
+						lru_in[2] = 1;
+					end
+					
 					lru_write = 1;
 					resp = 1;
 				end
@@ -99,18 +130,35 @@ begin : state_actions
 				// Write
 				if (cpu_write)
 				begin
+					lru_in = lru_out;
 					dirty_in = 1;
 					if (cline0_and)
 					begin
 						dirty0_write = 1;
 						data0_write = 1;
-						lru_in = 1;
+						lru_in[0] = 0;
+						lru_in[1] = 0;
 					end
 					else if (cline1_and)
 					begin
 						dirty1_write = 1;
 						data1_write = 1;
-						lru_in = 0;
+						lru_in[0] = 0;
+						lru_in[1] = 1;
+					end
+					else if (cline2_and)
+					begin
+						dirty2_write = 1;
+						data2_write = 1;
+						lru_in[0] = 1;
+						lru_in[2] = 0;
+					end
+					else if (cline3_and)
+					begin
+						dirty3_write = 1;
+						data3_write = 1;
+						lru_in[0] = 1;
+						lru_in[2] = 1;
 					end
 					
 					lru_write = 1;
@@ -137,24 +185,35 @@ begin : state_actions
 			valid_in = 1;
 			
 			if (mem_resp) begin
-				if (lru_out)
-				begin
-					tag1_write = 1;
-					valid1_write = 1;
-					dirty1_write = 1;
-					data1_write = 1;
-					//lru_in = 0;
-				end
-				else
+				if (lru_out[0] == 1 && lru_out[1] == 1)
 				begin
 					tag0_write = 1;
 					valid0_write = 1;
 					dirty0_write = 1;
 					data0_write = 1;
-					//lru_in = 1;
+				end
+				else if (lru_out[0] == 1 && lru_out[1] == 0)
+				begin
+					tag1_write = 1;
+					valid1_write = 1;
+					dirty1_write = 1;
+					data1_write = 1;
+				end
+				else if (lru_out[0] == 0 && lru_out[1] == 1)
+				begin
+					tag2_write = 1;
+					valid2_write = 1;
+					dirty2_write = 1;
+					data2_write = 1;
+				end
+				else
+				begin
+					tag3_write = 1;
+					valid3_write = 1;
+					dirty3_write = 1;
+					data3_write = 1;
 				end
 			end
-			//lru_write = 1;
 		end
 		
 		default: ;
--- l2_cache_datapath.sv
+++ l2_cache_datapath.sv
@@ -10,23 +10,33 @@ module l2_cache_datapath
 	
 	input valid0_write,
 	input valid1_write,
+	input valid2_write,
+	input valid3_write,
 	input valid_in,
 	input dirty0_write,
 	input dirty1_write,
+	input dirty2_write,
+	input dirty3_write,
 	input dirty_in,
 	input tag0_write,
 	input tag1_write,
+	input tag2_write,
+	input tag3_write,
 	input data0_write,
 	input data1_write,
+	input data2_write,
+	input data3_write,
 	input lru_write,
-	input lru_in,
+	input [2:0] lru_in,
 	input pmem_addr_sig,
 	input data_sig,
 	
-	output logic lru_out,
+	output logic [2:0] lru_out,
 	output logic dirty_out,
 	output logic cline0_and,
 	output logic cline1_and,
+	output logic cline2_and,
+	output logic cline3_and,
 	output logic hit,
 	output lc3b_data data_out,
 	output lc3b_data pdata_out,
@@ -43,19 +53,37 @@ assign offset = mem_address[3:1];
 
 logic valid0_out;
 logic valid1_out;
+logic valid2_out;
+logic valid3_out;
 
 logic dirty0_out;
 logic dirty1_out;
+logic dirty2_out;
+logic dirty3_out;
+logic dirty_10_out;
+logic dirty_32_out;
 
 lc3b_l2_tag tag0_out;
 lc3b_l2_tag tag1_out;
+lc3b_l2_tag tag2_out;
+lc3b_l2_tag tag3_out;
+lc3b_l2_tag tag_10_out;
+lc3b_l2_tag tag_32_out;
 lc3b_l2_tag tag_out;
 logic comp0_out;
 logic comp1_out;
+logic comp2_out;
+logic comp3_out;
+
+logic [1:0] cline_and;
 
 lc3b_data data_in;
 lc3b_data data0_out;
 lc3b_data data1_out;
+lc3b_data data2_out;
+lc3b_data data3_out;
+lc3b_data data_10_out;
+lc3b_data data_32_out;
 lc3b_data cpu_data_out;
 
 lc3b_word write_back_addr;
@@ -79,8 +107,26 @@ l2_array #(.width(1)) valid1
 	.dataout(valid1_out)
 );
 
+l2_array #(.width(1)) valid2
+(
+	.clk(clk),
+	.write(valid2_write),
+	.index(index),
+	.datain(valid_in),
+	.dataout(valid2_out)
+);
+
+l2_array #(.width(1)) valid3
+(
+	.clk(clk),
+	.write(valid3_write),
+	.index(index),
+	.datain(valid_in),
+	.dataout(valid3_out)
+);
+
 /* LRU Array */
-l2_array #(.width(1)) lru
+l2_array #(.width(3)) lru
 (
 	.clk(clk),
 	.write(lru_write),
@@ -108,11 +154,45 @@ l2_array #(.width(1)) dirty1
 	.dataout(dirty1_out)
 );
 
+l2_array #(.width(1)) dirty2
+(
+	.clk(clk),
+	.write(dirty2_write),
+	.index(index),
+	.datain(dirty_in),
+	.dataout(dirty2_out)
+);
+
+l2_array #(.width(1)) dirty3
+(
+	.clk(clk),
+	.write(dirty3_write),
+	.index(index),
+	.datain(dirty_in),
+	.dataout(dirty3_out)
+);
+
+mux2 #(.width(1)) dirty_mux10
+(
+	.sel(lru_out[1]),
+	.a(dirty1_out),
+	.b(dirty0_out),
+	.f(dirty_10_out)
+);
+
+mux2 #(.width(1)) dirty_mux32
+(
+	.sel(lru_out[2]),
+	.a(dirty3_out),
+	.b(dirty2_out),
+	.f(dirty_32_out)
+);
+
 mux2 #(.width(1)) dirty_mux
 (
-	.sel(lru_out),
-	.a(dirty0_out),
-	.b(dirty1_out),
+	.sel(lru_out[0]),
+	.a(dirty_32_out),
+	.b(dirty_10_out),
 	.f(dirty_out)
 );
 
@@ -135,6 +215,24 @@ l2_array #(.width(8)) tag1
 	.dataout(tag1_out)
 );
 
+l2_array #(.width(8)) tag2
+(
+	.clk(clk),
+	.write(tag2_write),
+	.index(index),
+	.datain(tag),
+	.dataout(tag2_out)
+);
+
+l2_array #(.width(8)) tag3
+(
+	.clk(clk),
+	.write(tag3_write),
+	.index(index),
+	.datain(tag),
+	.dataout(tag3_out)
+);
+
 comparator #(.width(8)) comp0
 (
 	.value0(tag0_out),
@@ -149,9 +247,37 @@ comparator #(.width(8)) comp1
 	.out(comp1_out)
 );
 
+comparator #(.width(8)) comp2
+(
+	.value0(tag2_out),
+	.value1(tag),
+	.out(comp2_out)
+);
+
+comparator #(.width(8)) comp3
+(
+	.value0(tag3_out),
+	.value1(tag),
+	.out(comp3_out)
+);
+
 assign cline0_and = comp0_out & valid0_out;
 assign cline1_and = comp1_out & valid1_out;
-assign hit = cline0_and | cline1_and;
+assign cline2_and = comp2_out & valid2_out;
+assign cline3_and = comp3_out & valid3_out;
+assign hit = cline0_and | cline1_and | cline2_and | cline3_and;
+
+always_comb
+begin
+	if (cline0_and)
+		cline_and = 2'b00;
+	else if (cline1_and)
+		cline_and = 2'b01;
+	else if (cline2_and)
+		cline_and = 2'b10;
+	else
+		cline_and = 2'b11;
+end
 
 /* Data Arrays */
 cpudata cpu_data_cal
@@ -188,28 +314,80 @@ l2_array data1
 	.dataout(data1_out)
 );
 
-mux2 #(.width(128)) cdata_mux
+l2_array data2
 (
-	.sel(cline0_and),
+	.clk(clk),
+	.write(data2_write),
+	.index(index),
+	.datain(data_in),
+	.dataout(data2_out)
+);
+
+l2_array data3
+(
+	.clk(clk),
+	.write(data3_write),
+	.index(index),
+	.datain(data_in),
+	.dataout(data3_out)
+);
+
+mux4 #(.width(128)) cdata_mux
+(
+	.sel(cline_and),
+	.a(data0_out),
+	.b(data1_out),
+	.c(data2_out),
+	.d(data3_out),
+	.f(data_out)
+);
+
+/* Address to Physical Memory*/
+mux2 #(.width(128)) pdata_mux10
+(
+	.sel(lru_out[1]),
 	.a(data1_out),
 	.b(data0_out),
-	.f(data_out)
+	.f(data_10_out)
+);
+
+mux2 #(.width(128)) pdata_mux32
+(
+	.sel(lru_out[2]),
+	.a(data3_out),
+	.b(data2_out),
+	.f(data_32_out)
 );
 
 mux2 #(.width(128)) pdata_mux
 (
-	.sel(lru_out),
-	.a(data0_out),
-	.b(data1_out),
+	.sel(lru_out[0]),
+	.a(data_32_out),
+	.b(data_10_out),
 	.f(pdata_out)
 );
 
-/* Address to Physical Memory*/
+mux2 #(.width(8)) tag_mux10
+(
+	.sel(lru_out[1]),
+	.a(tag1_out),
+	.b(tag0_out),
+	.f(tag_10_out)
+);
+
+mux2 #(.width(8)) tag_mux32
+(
+	.sel(lru_out[2]),
+	.a(tag3_out),
+	.b(tag2_out),
+	.f(tag_32_out)
+);
+
 mux2 #(.width(8)) tag_mux
 (
-	.sel(lru_out),
-	.a(tag0_out),
-	.b(tag1_out),
+	.sel(lru_out[0]),
+	.a(tag_32_out),
+	.b(tag_10_out),
 	.f(tag_out)
 );
 
